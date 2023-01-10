0516edac33ca17235060da0e9be2cb1b5f4bd0f7
AlexHoorn <12898097+AlexHoorn@users.noreply.github.com>
2023-01-10 23:19:44 +0100

    Convention

--- src/environment.py
+++ src/environment.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+from typing import Any
 
 import gymnasium as gym
 from gymnasium import spaces
@@ -7,28 +8,33 @@ from gymnasium import spaces
 class DiscreteDamEnv(gym.Env):
     """Dam Environment that follows gym interface"""
 
+    # static properties
+    max_stored_energy = 100000 * 1000 * 9.81 * 30  # U = mgh
+    min_stored_energy = 0
+    # a positive flow means emtpying the reservoir
+    max_flow_rate = 5 * 3600 * 9.81 * 30  # 5 m^3/s to m^3/h * gh
+
+    price_bin_size = 100
+
     def __init__(self, price_data: dict[datetime, float]):
-        # static properties
-        self.max_stored_energy = 100000 * 1000 * 9.81 * 30  # U = mgh
-        self.min_stored_energy = 0
-        # a positive flow means emtpying the reservoir
-        self.max_flow_rate = 5 * 3600 * 9.81 * 30  # 5 m^3/s to m^3/h * gh
-        self.price_bin_size = 100
+        super().__init__()
 
         self.price_data = price_data
 
         self.reset()
 
         # 0 = do nothing
-        # 1 = empty
-        # 2 = fill
+        # 1 = empty / sell
+        # 2 = fill / buy
         self.action_space = spaces.Discrete(3)
 
         # state is (hour, electricity price (bins))
         n_bins = int(max(self.price_data.values()) // self.price_bin_size)
         self.observation_space = spaces.MultiDiscrete([24, n_bins])
 
-    def reset(self):
+    def reset(self, *, seed: int | None = None, options: dict[str, Any] | None = None):
+        super().reset(seed=seed)
+
         self.stored_energy = self.max_stored_energy
         self.current_date = min(self.price_data.keys())
         self.current_price = self.price_data[self.current_date]
@@ -52,6 +58,7 @@ class DiscreteDamEnv(gym.Env):
         # move to the next hour
         self._set_next_state()
 
+        # TODO: does not fully follow Gym step convention
         return self._get_state(), self._get_reward(flow_rate)
 
     def _constrain_flow_rate(self, flow_rate: float):
