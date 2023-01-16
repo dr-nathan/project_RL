67143fca616aecaed2b94562b20b1789d3a5e927
Teresa Liberatore <terryliberatore@gmail.com>
2023-01-16 15:48:09 +0100

    Update run_baseline.py

--- run_baseline.py
+++ run_baseline.py
@@ -4,5 +4,5 @@ from src.baseline_stupid import Baseline
 
 df = pd.read_excel("./data/train.xlsx")
 val = pd.read_excel("./data/validate.xlsx")
-a = Baseline(df=df, low_perc=0.2, medium_perc=0.07, val=val)  #
+a = Baseline(df=df, low_perc=0.2, medium_perc=0.07, val=df)  #
 a.plot_baseline()
