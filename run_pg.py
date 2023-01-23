9447e83c2a9d85561022739007b050fd3e79e1a8
Alex Hoorn <alex@floorganise.com>
2023-01-23 17:10:56 +0100

    Update PG

--- run_pg.py
+++ run_pg.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import pandas as pd
 
 from src.agent_continuous import PolicyGradientAgent
-from src.environment import DiscreteDamEnv
+from src.environment import ContinuousDamEnv
 from src.utils import convert_dataframe
 
 if __name__ == "__main__":
@@ -15,25 +15,19 @@ if __name__ == "__main__":
     val_data = pd.read_excel(Path(__file__).parent / "data" / "validate.xlsx")
     val_data = convert_dataframe(val_data)
 
-    # determine quantile to cap the price for the bins at
-    # price_quantile = pd.Series(train_data.values()).quantile(0.99)
-
     # create environment and agent
-    environment = DiscreteDamEnv(train_data, 200)
+    environment = ContinuousDamEnv(train_data)
     agent = PolicyGradientAgent(learning_rate=0.1, env=environment)
 
     # train agent
-    epsilon_decay = True
-    epsilon = 0.2  # overriden if epsilon_decay is True
-    alpha = 0.3
-    n_episodes = 20
+    n_episodes = 10
     random_startpoint = False
 
     episode_data = agent.train(n_episodes)
 
-    agent.env.plot_price_distribution()
-    agent.env.episode_data.plot_debug("Final training episode")
+    # agent.env.plot_price_distribution()
+    agent.env.episode_data.debug_plot("Final training episode")
 
     # validate agent
     agent.validate(price_data=val_data)
-    agent.env.episode_data.plot_debug("Validation episode")
+    agent.env.episode_data.debug_plot("Validation episode")
